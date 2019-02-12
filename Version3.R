a353227e46c53a615e3b1bfdf10b0ad030665dae
Chaitanya Rastogi <crastogi@gmail.com>
2019-02-11 22:59:40 -0500

    Updates to version3; still working on improving the performance of 2D wang-landau implementation

--- Version3.R
+++ Version3.R
@@ -1,5 +1,4 @@
 # Must install RapidScore C++ Library First!
-#source("~/Documents/Research/SequenceOptimization/SpecificityOptimization.R")
 library(RapidScore)
 source("~/Documents/Research/SELEX/MultinomialPaper/Submissions/PNAS/Figures/plotting_format.R")
 all.models = load.hox.models()
@@ -19,143 +18,171 @@ a.to.n.conversion = c(0:3)
 names(a.to.n.conversion) = c("A","C","G","T")
 n.to.a.conversion = c("A","C","G","T")
 
-# biased wang-landau 
-wang.landau = function(score.object, start.seq, cost.function, verbose, mutation.penalty) {
-  # Size of energy divisions
+# biased 2D wang-landau (looks only for positive mutations)
+wang.landau = function(score.object, start.seq, cost.function, isBiased=TRUE, verbose=TRUE) {
+  verbose = FALSE
+  # Size of energy divisions and other parameters - play with these?
   energy.div.size = 1
   f = 1
   minCount = 100
   minIters = 10000
   fCrit = .9
   tol = .5
-  quantile.cutoff=.75
-  
-  # Convert sequence to base 4 encoding
+
+  # Convert sequence to base 4 encoding, and define positions to mutate
   seq.len = length(start.seq)
-  #seq.pos = 1:seq.len
-  seq.pos = 1:(seq.len-1)
-  max.mutate.bases = 1 #1:round(seq.len/20)
+  seq.pos = 1:seq.len
   xn = rep(0, seq.len)
   for (i in 1:seq.len) {
     xn[i] = a.to.n.conversion[start.seq[i]]
   }
   base.seq = xn
-  curr.opt.seq = xn
   
-  # Compute starting value
-  scores = score.object$scoreBulk(curr.opt.seq)
-  nModels = length(scores)
+  # Compute starting values
+  scores = score.object$scoreBulk(base.seq)
   base.scores = scores
-  start.cost = cost.function(scores, base.scores, 0)
+  start.cost = cost.function(scores, base.scores)
   En = start.cost
-  curr.opt.cost = En
   if (verbose) {
     cat(paste0("Starting Cost: ", start.cost, "\n"))
-  }
-  
-  if (mutation.penalty!=0) {
-    # Compute penalty based on the distribution of (positive) changes 
-    score.dist = NULL
-    for (i in 1:seq.len) {
-      for (curr.base in 0:3) {
-        # Skip for the same base
-        if (curr.base==base.seq[i]) {
-          next
-        }
-        test.seq = base.seq
-        test.seq[i] = curr.base
-        scores = score.object$scoreBulk(test.seq)
-        test.cost = cost.function(scores, base.scores, 0)-start.cost
-        if (test.cost > 0) {
-          score.dist = c(score.dist, test.cost)
-        }
-      }
-    }
-    # Find percentile value from score distribution and set mutation penalty to it
-    mutation.penalty = as.numeric(quantile(score.dist, quantile.cutoff))
-    print(mutation.penalty)
   }
   
   # Initialize Wang-Landau scheme
   Emin = floor(min(0, En)/energy.div.size)*energy.div.size
   Emax = ceiling(max(0, En)/energy.div.size)*energy.div.size
-  nDivs= ((Emax-Emin)/energy.div.size+1)
-  H = rep(0, nDivs)
-  S = rep(0, nDivs)
+  MutMin = 0
+  MutMax = 2
+  eDivs= ((Emax-Emin)/energy.div.size+1)
+  mDivs = MutMax+1
+  H = matrix(data = 0, ncol = eDivs, nrow = mDivs)
+  Hidx = 3:mDivs      # Define the range of H indices we want to consider (here it starts from an edit distance of 2)
+  S = H
   x0= xn
   E0= En
-  I0= round((E0-Emin)/energy.div.size)+1
+  IE0= round((E0-Emin)/energy.div.size)+1
+  IM0= 1  # Initial bin for edit distance is 0 
+  opt.seqs = data.frame(Mutations=MutMin:MutMax, OptimalCost=-Inf)
+  opt.seqs$OptimalSeq=list(NA)
+  opt.seqs$OptimalCost[1] = E0
+  opt.seqs$OptimalSeq[[1]]= x0
   iters = 0
   
   # Loop continuously until convergence
   while (TRUE) {
-    # Propose a new state
+    # Propose a new state with a dinucleotide mutation (type of move does not seem to matter)
     xn = x0
     mutate.pos = sample(seq.pos, 1)
-    xn[mutate.pos:(mutate.pos+1)] = sample(0:3, 2, replace=T)
-    # nMuts = sample(max.mutate.bases, 1)
-    # mutate.pos = sample(seq.pos, nMuts)
-    # xn[mutate.pos] = sample(1:4, nMuts, replace=T)
+    xn[mutate.pos] = sample(0:3, 1)
     # Compute new cost
     scores = score.object$scoreBulk(xn)
-    En = cost.function(scores, base.scores, mutation.distance = mutation.penalty*sum(xn!=base.seq))
-    # See if the latest sequence is the best one
-    if (En > curr.opt.cost) {
-      curr.opt.cost = En
-      curr.opt.seq = xn
+    En = cost.function(scores, base.scores)
+    # # Biased sampling: Reject if sequence energy is less than the least value
+    # if (isBiased && En < Emin) {
+    #   next
+    # }
+    # Compute mutation distance; Reverse complement symmetry must be accounted for when computing
+    Mn = min(sum(xn!=base.seq), sum(rev(-1*(xn-3))!=base.seq)) 
+    # Compute new cost and mutation indexes
+    IEn = round((En-Emin)/energy.div.size)+1
+    IMn = Mn+1
+    # Update ranges and restart wang-landau process if a sequence is produced that is out of range
+    m.range.update = FALSE
+    e.range.update = FALSE
+    # Check mutation ranges
+    if (IMn > mDivs) {
+      m.range.update = TRUE
+      if (verbose) {
+        cat(paste0("Old Max Mutation: ", MutMax, "; New Max Mutation: ", IMn-1, "; "))
+      }
+      # Update and add rows to opt.seq
+      for (k in (MutMax+1):(IMn-1)) {
+        opt.seqs = rbind(opt.seqs, rep(c(k, -Inf, list(NA))))
+      }
+      # Recompute Range and divs
+      MutMax = IMn-1
+      mDivs = IMn
+      Hidx = 3:mDivs
     }
-    if (En < Emin) {
-      next
-    }
-    # Compute new index
-    In = round((En-Emin)/energy.div.size)+1
-    # Update range and restart wang-landau process if a sequence is produced that is out of range
-    if (In<=0 || In > nDivs) {
-      cat(paste0("Old Range: ", Emin, ":", Emax,"; "))
-      # Recompute range
+    # Check cost ranges
+    if (IEn<=0 || IEn > eDivs) {
+      e.range.update = TRUE
+      if (verbose) {
+        cat(paste0("Old Cost Range: ", Emin, ":", Emax,"; "))
+      }
+      # Recompute range and divs
       Emin.new = floor(min(Emin, En)/energy.div.size)*energy.div.size
       Emax.new = ceiling(max(Emax, En)/energy.div.size)*energy.div.size
-      cat(paste0("New Range: ", Emin.new, ":", Emax.new))
-      # Restart Wang-landau
+      if (verbose) {
+        cat(paste0("New Cost Range: ", Emin.new, ":", Emax.new))
+      }
+      cat(paste0("New Cost Range: ", Emin.new, ":", Emax.new, "\n"))
+    }
+    # Update range and restart wang-landau process if necessary
+    if (m.range.update || e.range.update) {
+      # Do not need to restart
       if (f==1) {
-        # Do not need a hard restart
         minH = min(H[H>0])
-        H = c(rep(0, Emin-Emin.new), H, rep(0, Emax.new-Emax))
-        S = c(rep(0, Emin-Emin.new), S, rep(0, Emax.new-Emax))
-        nDivs= length(H)
-        Emin = Emin.new
-        Emax = Emax.new
-        In = round((En-Emin)/energy.div.size)+1
-        cat(paste0("; Total Iterations: ", iters, "; mean: ", mean(H[H>0]), "; sd: ", sd(H[H>0]), "; min: ", minH, "\n"))
+        # Begin by padding the H and S matrices with mutation range increases
+        if (m.range.update) {
+          H = rbind(H, matrix(data = 0, ncol = ncol(H), nrow = mDivs-nrow(H)))
+          S = rbind(S, matrix(data = 0, ncol = ncol(S), nrow = mDivs-nrow(S)))
+        }
+        # Pad H and S with energy range increases
+        if (e.range.update) {
+          H = cbind(matrix(data = 0, ncol = Emin-Emin.new, nrow = nrow(H)), H, matrix(data = 0, ncol = Emax.new-Emax, nrow = nrow(H)))
+          S = cbind(matrix(data = 0, ncol = Emin-Emin.new, nrow = nrow(S)), S, matrix(data = 0, ncol = Emax.new-Emax, nrow = nrow(S)))
+          eDivs = ncol(H)
+          Emin = Emin.new
+          Emax = Emax.new
+          IEn = round((En-Emin)/energy.div.size)+1
+        }
+        if (verbose) {
+          cat(paste0("; Total Iterations: ", iters, "; mean: ", mean(H[H>0]), "; sd: ", sd(H[H>0]), "; min: ", minH, "\n"))
+        }
+        # print(dim(H))
       } else {
-        # Hard restart required
-        Emin = Emin.new
-        Emax = Emax.new
-        nDivs= ((Emax-Emin)/energy.div.size+1)
-        H = rep(0, nDivs)
-        S = rep(0, nDivs)
+        # Need a hard restart
+        if (e.range.update) {
+          Emin = Emin.new
+          Emax = Emax.new
+          eDivs =((Emax-Emin)/energy.div.size+1)
+        }
+        mDivs = MutMax+1
+        H = matrix(data = 0, ncol = eDivs, nrow = mDivs)
+        S = H
         x0= xn
         E0= En
-        I0= round((E0-Emin)/energy.div.size)+1
+        IE0= round((E0-Emin)/energy.div.size)+1
+        IM0= IMn
         iters = 0
         f = 1
         cat("; Restarted sampling!\n")
         next
       }
     }
+    # See if the latest sequence is the best one; if so, update list of sequences
+    if (En > opt.seqs$OptimalCost[IMn]) {
+      opt.seqs$OptimalCost[IMn] = En
+      opt.seqs$OptimalSeq[[IMn]] = xn
+    }
     # Compute latest acceptance
-    a =exp(S[I0] - S[In])
+    a =exp(S[IM0, IE0] - S[IMn, IEn])
     # Metropolis-Hastings step
     if (runif(1) <= a) {
       # Accept current proposal
       x0 = xn
       E0 = En
-      I0 = In
+      IE0 = IEn
+      IM0 = IMn
     }
     # Update
-    S[I0] = S[I0] + f
-    H[I0] = H[I0] + 1
+    S[IM0, IE0] = S[IM0, IE0] + f
+    H[IM0, IE0] = H[IM0, IE0] + 1
     iters = iters + 1
+    
+    # print(iters)
+    # print(H)
+    
     # Ensure that all energy bins have been visited an even number of times
     if (min(H[H>0]) > minCount) {
       if (min(H[H>0]) >= fCrit*mean(H[H>0])) {
@@ -166,27 +193,39 @@ wang.landau = function(score.object, start.seq, cost.function, verbose, mutation
           break
         }
         iters = 0;
-        H = rep(0, nDivs)
+        H = matrix(data = 0, ncol = eDivs, nrow = mDivs)
       }
+    }
+    if (iters %% 50000 ==0) {
+      cat(paste0("Total Iterations: ", iters, "; mean: ", mean(H[H>0]), "; sd: ", sd(H[H>0]), "; min: ", min(H[H>0]), "\n"))
+      print(rowSums(H))
+      print(apply(H, 1, FUN=function(x) min(x[x>0])))
+    }
+    if (iters == 1E5) {
+      break
     }
   }
   
   # Convert optimal sequence to string representation
-  opt.seq = start.seq
-  for (i in 1:seq.len) {
-    opt.seq[i] = n.to.a.conversion[curr.opt.seq[i]+1]
+  for (i in 1:nrow(opt.seqs)) {
+    curr.seq = opt.seqs$OptimalSeq[[i]]
+    if (sum(is.na(curr.seq))>0) {
+      next
+    }
+    out.seq = start.seq
+    for (j in 1:seq.len) {
+      out.seq[j] = n.to.a.conversion[curr.seq[j]+1]
+    }
+    out.seq = paste0(out.seq, collapse="")
+    opt.seqs$OptimalSeq[[i]] = out.seq
   }
-  opt.seq = paste0(opt.seq, collapse="")
-  return(c(start.cost, opt.seq, curr.opt.cost))
+  opt.seqs$OptimalSeq = unlist(opt.seqs$OptimalSeq)
+  print(opt.seqs)
+  return(opt.seqs)
 }
 
 # Sequence optimizer. REQUIRES a seed sequence. All scores are automatically adjusted to accomodate the TOTAL sequence score
-optimal.sequence = function(..., model.list = NA, cost.function, seq.len = NA, seed.seq,
-                            return.max.only = TRUE, verbose = FALSE, penalize.mutations = FALSE) {
-  warm.start.multiple = 25
-  mutate.dinucs = TRUE
-  use.wang.landau = TRUE
-  
+optimal.sequence = function(..., model.list = NA, cost.function, seq.len = NA, seed.seq, verbose = FALSE) {
   if (is.na(model.list)) {
     input.models = list(...)
   } else {
@@ -226,29 +265,11 @@ optimal.sequence = function(..., model.list = NA, cost.function, seq.len = NA, s
     cat(paste0("Sequence length to optimize: ",seq.len,"; Left Random Field: ", l.len,", Right Random Field: ", r.len, "\n"))
   }
   
-  # Set mutation penalty 
-  if (!penalize.mutations) {
-    mutation.penalty = 0
-  } else {
-    mutation.penalty = 1
-  }
-  
-  run.outputs = NULL
   # Optimize using wang-landau sampling. Construct starting sequence
   start.seq = c(sample(c("A","C","G","T"), l.len, replace = TRUE), seed.seq, sample(c("A","C","G","T"), r.len, replace = TRUE))
   # Run optimization
-  output = wang.landau(score.object, start.seq, cost.function, verbose, mutation.penalty)
-  run.outputs = rbind(run.outputs, c(paste(start.seq, collapse=""), output))
-  run.outputs = data.frame(run.outputs, stringsAsFactors = FALSE)
-  names(run.outputs) = c("StartSequence", "StartCost", "EndSequence", "EndCost")
-  
-  print(run.outputs)
-  
-  if (return.max.only) {
-    return(run.outputs[which(run.outputs$EndCost==max(run.outputs$EndCost))[1],])
-  } else {
-    return(run.outputs)
-  }
+  output = wang.landau(score.object, start.seq, cost.function, isBiased = TRUE, verbose = verbose)
+  return(output)
 }
 
 #############################################################
@@ -275,23 +296,20 @@ genome.profile = function(sequence) {
 }
 
 # simple cost function: Maintain/improve the affinity of the first protein, drop the affinity of the rest
-cost.fun = function(scored.list, base.scores, mutation.distance) {
+cost.fun = function(scored.list, base.scores) {
   cost = log(sum(exp(scored.list[[1]]))/sum(exp(base.scores[[1]]))) - 
-    barrier.func(1000, 1, exp(max(scored.list[[1]]))/10, 
+    barrier.func(50, 1, exp(max(scored.list[[1]]))/10, 
                  .85*exp(max(base.scores[[1]]))-exp(max(scored.list[[1]])))
   for (i in 2:length(scored.list)) {
     cost = cost + log(sum(exp(base.scores[[i]]))/sum(exp(scored.list[[i]])))
   }
-  return(cost-mutation.distance)
+  return(cost)
 }
 
 # Optimize the fkh250 sequence
 fkh250 = "CCTCGTCCCACAGCTGGCGATTAATCTTGACATTGAG"
 # Loop over all proteins
-final.seqs = fkh250
-final.costs = 0
-final.seqs.p = fkh250
-final.costs.p = 0
+output.profiles = list()
 for (i in 1:length(input.models)) {
   # Select a single model
   idx = 1:length(input.models)
@@ -302,8 +320,7 @@ for (i in 1:length(input.models)) {
   final.seqs = c(final.seqs, out$EndSequence)
   final.costs= c(final.costs,out$EndCost)
   out = optimal.sequence(model.list = curr.list, cost.function = cost.fun, seed.seq = fkh250, verbose = TRUE, penalize.mutations = TRUE)
-  final.seqs.p = c(final.seqs.p, out$EndSequence)
-  final.costs.p= c(final.costs.p,out$EndCost)
+  output.profiles = c(output.profiles, out)
 }
 
 # Now compute final affinities 
